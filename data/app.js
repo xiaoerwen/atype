ca7877322b06cc1117ca22ef106a2716e37136ae
caixiaowen <caixiaowen@baidu.com>
2018-09-07 19:56:10 +0800

    reconstruct logic of codes

--- data/app.js
+++ data/app.js
@@ -1,197 +1,206 @@
 import express from 'express';
-const cheerio = require('cheerio');
-const superagent = require('superagent');
-const async = require('async');
-const fs = require('fs');
-const path = require('path');
+import cheerio from 'cheerio';
+import superagent from 'superagent';
+import mapLimit from 'async/mapLimit';
+import fs from 'fs';
+import path from 'path';
+import { DICT } from './config.js';
+const { 
+    host, 
+    mainPage, 
+    categoryClass, 
+    apiTable,
+    paramTable,
+    apiHtml,
+    apiTitle,
+    apiTitleAnother,
+    apiParamTable,
+    paramColumn,
+    neccessColumn,
+    urlProp,
+    fileName
+} = DICT;
 const app = express();
 
-// const formData = require('./form.js'); // 转化数据格式
+let apiData = [];
 
-import { DICT } from './config.js';
-const host = DICT.host;
-const mainPage = DICT.mainPage;
-
-let api_url = [];
-
-// 第一步：从某个页面的chapter抓取api
-function findData() {
-    return new Promise((resolve, reject) => {
+// 第一步：从页面的chapter栏抓取API下各个分类
+function findApiCaterioges() {
+    return new Promise((resolve) => {
         superagent.get(host + mainPage) // 请求页面地址
-        .buffer(true)
         .end((err, sres) => { // 页面获取到的数据
             if (err) {
-                reject(err);
+                throw err;
             }
-            console.log(111, '开始抓取数据啦～');
+            console.log(100, '开始抓取API下的分类啦～');
 
             let $ = cheerio.load(sres.text); // 用cheerio解析页面数据
-            let $api = $('.m-doc-sidebar-nav-selected[data-name|="api"] > ul > li');
+            let $api = $(categoryClass);
 
             $api.each((index, element) => {
-                let $eleItem = $(element).find('a');
-                api_url.push({
-                    title: $eleItem.attr('href'),
+                let $eleItem = $(element).find(apiHtml);
+                apiData.push({
+                    title: $eleItem.attr(urlProp),
                     content: []
                 });
             });
-            // 依次读取每个chapter的api
-            findTable().then(() => {
-                console.log(888, '抓取成功！');
-                resolve();
-            }).catch((err) => {
-                console.log(err);
-                throw err;
-            });
+            resolve();
         });
     });
 }
 
-// 第三步：读取表格里的api
-function readTable(url) {
-    return new Promise((resolve, reject) => {
-        superagent.get(host + url.title)
-        .end((err, sres) => {
-            console.log(100, '开始抓表格啦～');
-            let item = [];
-            if (err) {
-                reject(err);
-            }
-
-            let $ = cheerio.load(sres.text);
-            $('.article-entry tbody tr').each((index, element) => {
-                let $el = $(element).find('a');
-                let it = {
-                    title: $el.text(),
-                    href: $el.attr('href'),
-                    param: []
-                }
-                item.push(it);
-            });
-            url.content = item;
-            resolve(url);
-        });
-    });
-}
-
-// 第二步：对应每个api表格
-function findTable() {
+// 第二步：依次抓取每个分类下的所有API
+function findApi() {
     return new Promise((resolve) => {
         let count = 0;
-        async.mapLimit(api_url, 100, (url) => {
+        // 并发处理多个请求
+        mapLimit(apiData, 100, (category) => {
             count++;
-            console.log('现在的并发数是', count, '，正在抓取的是', url);
-            readTable(url).then((res) => {
-                console.log(333, '读取表格成功！');
-                url = res;
-                return findParam(res.content);
-            }).then(() => {
-                count--;
-                if (count == 0) {
-                    resolve();
+            console.log('现在的并发数是', count, '，正在抓取的是', category);
+            superagent.get(host + category.title)
+            .end((err, sres) => {
+                console.log(101, '开始读取分类下的API啦～');
+                let item = [];
+                if (err) {
+                    throw err;
                 }
-            }).catch((err) => {
-                throw err;
-            });
-        });
-    });
-}
 
-// 第五步：读取必需参数
-function readParam (obj, href) {
-    return new Promise((resolve, reject) => {
-        superagent.get(href)
-        .end((err, sres) => {
-            if (err) {
-                reject(err);
-            }
-
-            let $ = cheerio.load(sres.text);
-            if (obj.title == "requestPolymerPayment 百度电商开放平台：产品介绍") {
-                obj.title = 'requestPolymerPayment';
-            }
-            let title = obj.title;
-            let p = /[\.\,\(\)]/g;
-            if (p.test(title)) {
-                title = title.replace(p, '-');
-                let p3 = /[\-*]$/g;
-                while (p3.test(title)) {
-                    title = title.replace(p3, '');
-                }
-                let p2 = /[\[\]\ ]/g;
-                if (p2.test(title)) {
-                    title = title.replace(p2, '');
-                }
-            }
-            let $el = $('#' + title);
-            let hasH2 = $el.nextAll().filter('h2');
-            let $hasTable;
-            if (hasH2) {
-                let hasH3 = $el.nextUntil('h2').filter('h3');
-                if (hasH3) {
-                    $hasTable = $el.nextUntil('h3').filter('table');
-                } else {
-                    $hasTable = $el.nextUntil('h2').filter('table');
-                }
-            } else {
-                let hasH3 = $el.nextAll().filter('h3');
-                if (hasH3) {
-                    $hasTable = $el.nextUntil('h3').filter('table');
-                } else {
-                    $hasTable = $el.nextAll().filter('table');
-                }
-            }
-            if ($hasTable) {
-                let par = [];
-                $hasTable.first().find('tbody > tr').each((index, element) => {
-                    console.log(1010, '开始读取参数～');
-                    let $param = $(element).find('td:nth-of-type(1)');
-                    let $neccess = $(element).find('td:nth-of-type(3)');
-                    if ($neccess.text() == '是') {
-                        par.push($param.text());
+                let $ = cheerio.load(sres.text);
+                $(apiTable).each((index, element) => {
+                    let $el = $(element).find(apiHtml);
+                    let it = {
+                        title: $el.text(),
+                        href: $el.attr(urlProp),
+                        param: []
                     }
+                    item.push(it);
                 });
-                obj.param = par;
-            }       
-            console.log(1212, obj);
-            resolve(obj);
-        });
-    });
-}
-
-// 第四步：
-function findParam(content) {
-    return new Promise((resolve) => {
-        let count = 0;
-        async.mapLimit(content, 100, (url) => {
-            count++;
-            console.log('现在的并发数是', count, '，正在抓取的是', url);
-            readParam(url, url.href).then((res) => {
-                url = res;
-                return res;
-            }).then(() => {
+                category.content = item;
                 count--;
-                console.log(666, '读取参数成功！');
                 if (count == 0) {
                     resolve();
                 }
-            }).catch((err) => {
-                throw err;
             });
         });
     });
 }
 
-app.get('/', (req, res, next) => {
-    findData().then(() => {
-        fs.writeFileSync(path.join(__dirname, 'app.json'), JSON.stringify(api_url, null, 2));
-        console.log(999, '写入成功');
-        // formData.form();  // 转化数据格式
+// 判断是否必需参数
+function isNeccessParam ($, obj) {
+    // 文档中唯一一条例外数据
+    if (obj.title == "requestPolymerPayment 百度电商开放平台：产品介绍") {
+        obj.title = 'requestPolymerPayment';
+    }
+    let title = obj.title;
+    // 特殊符号需转成'-'，首尾不要有特殊符号
+    let p = /[\.\,\(\)]/g;
+    if (p.test(title)) {
+        title = title.replace(p, '-');
+        let p3 = /[\-*]$/g;
+        while (p3.test(title)) {
+            title = title.replace(p3, '');
+        }
+        let p2 = /[\[\]\ ]/g;
+        if (p2.test(title)) {
+            title = title.replace(p2, '');
+        }
+    }
+    let $el = $('#' + title);
+    // API多为h2标题
+    let hasH2 = $el.nextAll().filter(apiTitle);
+    let $hasTable;
+    if (hasH2) {
+        // API为h2标题时，需保证不受下面其他API的影响
+        let hasH3 = $el.nextUntil(apiTitle).filter(apiTitleAnother);
+        if (hasH3) {
+            $hasTable = $el.nextUntil(apiTitleAnother).filter(apiParamTable);
+        } else {
+            $hasTable = $el.nextUntil(apiTitle).filter(apiParamTable);
+        }
+    } else {
+        // 有的API为h3标题
+        let hasH3 = $el.nextAll().filter(apiTitleAnother);
+        // API为h3标题时，也需保证不受下面其他API的影响
+        if (hasH3) {
+            $hasTable = $el.nextUntil(apiTitleAnother).filter(apiParamTable);
+        } else {
+            $hasTable = $el.nextAll().filter(apiParamTable);
+        }
+    }
+    // 参数描述写在表格中，所以从表格读取
+    if ($hasTable) {
+        let par = [];
+        $hasTable.first().find(paramTable).each((index, element) => {
+            let $param = $(element).find(paramColumn);
+            let $neccess = $(element).find(neccessColumn);
+            if ($neccess.text() == '是') {
+                par.push($param.text());
+            }
+        });
+        obj.param = par;
+    }
+    return obj;    
+}
+
+// 读取单个API的必需参数
+function findParamOfApi(content) {
+    return new Promise((resolve) => {
+        let count = 0;
+        mapLimit(content, 100, (api) => {
+            count++;
+            console.log('现在的并发数是', count, '，正在抓取的是', api);
+            superagent.get(api.href)
+            .end((err, sres) => {
+                if (err) {
+                    throw err;
+                }
+
+                console.log(103, '开始抓取每个API的参数啦～');
+                let $ = cheerio.load(sres.text);
+                // 判断是否必需参数
+                api = isNeccessParam($, api);
+                count--;
+                if (count == 0) {
+                    resolve(count);
+                }
+            });
+        }, (err) => {
+            throw err;
+        });
+    });
+}
+
+// 第三步：遍历所有API各自的必需参数
+function mapApiForParam() {
+    let allApi = [];
+    let len = apiData.length;
+    for(let i = 0; i < len; i++) {
+        // 挨个读取API的必需参数
+        allApi.push(findParamOfApi(apiData[i].content));
+    }
+    // 必须等到数据全部抓完才返回结果
+    return Promise.all(allApi);
+}
+
+app.get('/', (req, res) => {
+    // 第一步：从页面的chapter栏抓取API下各个分类
+    findApiCaterioges().then(() => {
+        // 第二步：依次抓取每个分类下的所有API
+        return findApi();
+    }).then(() => {
+        console.log(102, '抓取全部API成功！');
+        // 第三步：抓取所有API各自的必需参数
+        return mapApiForParam();
+    }).then(() => {
+        console.log(104, '抓取各个API的参数成功！');
+        // 第四步：将数据写入文件
+        fs.writeFileSync(path.join(__dirname, fileName), JSON.stringify(apiData, null, 2));
+        console.log(105, '写入文件成功');
+        res.send('开始读取数据...');
     }).catch((err) => {
+        console.log(err);
         throw err;
     });
-
-    res.send('读取数据成功');
 });
 
 app.listen(8811, () => {
--- data/config.js
+++ data/config.js
@@ -1,4 +1,26 @@
 export const DICT = {
-    host: 'https://smartprogram.baidu.com',
-    mainPage: '/docs/develop/framework/app-service_getcurrentpages/'
+    host: 'smartprogram.baidu.com',
+    // 初始页面路径
+    mainPage: '/docs/develop/framework/app-service_getcurrentpages/',
+    // API总列下的分类
+    categoryClass: '.m-doc-sidebar-nav-selected[data-name|="api"] > ul > li',
+    // 每个分类下的API表格
+    apiTable: '.article-entry tbody tr',
+    // 每个API下的参数表格
+    paramTable: 'tbody > tr',
+    // 现在存放API都是a标签
+    apiHtml: 'a',
+    // API大多为h2标题，部分为h3标题
+    apiTitle: 'h2',
+    apiTitleAnother: 'h3',
+    // 参数放在表格里
+    apiParamTable: 'table',
+    // 表格第一列为参数名
+    paramColumn: 'td:nth-of-type(1)',
+    // 表格第三列为是否必填
+    neccessColumn: 'td:nth-of-type(3)',
+    // 读取的url存放在a标签的href属性里
+    urlProp: 'href',
+    // 最终存储的文件名称
+    fileName: 'app.json'
 };
